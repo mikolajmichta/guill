de0de46636326d869935c7cdeb8a8179f3b41216
mikolajmichta <hansolo92@wp.pl>
2018-06-17 15:27:39 +0200

    added more regexp

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,5 @@
 function guill () {
-    const regularExp = /królow.+elżbiet.|książ.+karol|księc.+karol|kat.+middleton|megha.+markle|księż.+kate|książ.+harr|księc.+harr|książ.+william|księc.+william/ig
+    const regularExp = /królow.+elżbiet.|książ.+karol|księc.+karol|kat.+middleton|megha.+markle|księż.+kate|książ.+harr|księc.+harr|książ.+william|księc.+william|księż.+megh|królo.+megh/i
 
     let royalNodes = $("span").filter(function () {
         return regularExp.test($(this).text());
